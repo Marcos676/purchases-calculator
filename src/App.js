cfc747885030f0442ed5d02c6e05b6e6243a29fb
Marcos676 <patino-marcos@outlook.com>
2025-01-01 15:58:44 -0300

    Agrego modal estatico

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ import TotalPrice from "./components/TotalPrice";
 function App() {
   return (
     <div className="App">
+
+    <div className="modal">
+
+      <div className="container-modal-form">
+        <form>
+        <button className="btn-close">X</button>
+          <h2 className="modal-title">Nuevo producto</h2>
+          <input className="w-100" type="text" placeholder="Nombre"/>
+          <input className="w-100" type="text"  placeholder="Descripciòn"/>
+          <input className="w-100" type="number" placeholder="Precio"/>
+          <input className="w-100" type="number" placeholder="Descuento %"/>
+          <input className="w-100" type="number" placeholder="Cantidad"/>
+          <button className="btn-save w-70">GUARDAR</button>
+        </form>
+      </div>
+
+    </div>
+
       <Header />
       <main> 
         <ProductList />
